270f9c174fcc66807485508e3644bb595ed6b7ed
Ayuuuu1212121 <oxfordliner78@gmail.com>
2026-08-13 08:47:03 -0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "sex style ?", // required by Discord API, not shown in the client
         state: "He loves you <3",     // this is what people actually see
         type: 2,               // Custom
       },
